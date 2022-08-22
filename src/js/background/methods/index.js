86c370d45d75682394e5fd085a70b1f1c9a86ed4
Julian Dominguez-Schatz <juliands@stripe.com>
2022-08-22 11:43:00 -0700

    Refactor to have a cleaner backend code structure

---
+++ src/js/background/methods/index.js
@@ -0,0 +1,13 @@
+import addRule from "./addRule";
+import getRules from "./getRules";
+import addRules from "./addRules";
+import deleteRule from "./deleteRule";
+import replaceRule from "./replaceRule";
+
+export default {
+  addRule,
+  addRules,
+  deleteRule,
+  getRules,
+  replaceRule,
+};
